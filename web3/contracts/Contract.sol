44c46278d58a6dcf7adea80155e53d8e60b2facb
Calm Andy <thought7878@qq.com>
2024-01-24 18:26:26 +0800

    feat(web3): add contract

--- web3/contracts/Contract.sol
+++ web3/contracts/Contract.sol
@@ -1,6 +1,90 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract CrowdFunding {
+    // JS object
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        // donation amount of every donator
+        uint256[] donations;
+    }
+
+    // campaigns[0], point to a compaign
+    mapping(uint256 => Campaign) public campaigns;
+    // create a global variable
+    uint public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        // is everything ok? no, code is not going to proceed 继续
+        require(campaign.deadline < block.timestamp, "deadline passed!");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        // the donate amount from front
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        //
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    /**
+     * get a list of people that donated a campaign, and donation amount
+     *
+     * @param _id campaign id
+     * @return address[] donators
+     * @return uint256[] donations
+     */
+    function getDonaters(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    // get a list of all campaigns
+    function getCampaigns() public view returns (Campaign[] memory) {
+        // Campaign array: [{},{}]
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
